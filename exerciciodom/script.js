a116676e2bbc2bc1a59a74fc52ecf0212107d963
JeskaLopes <jessica97cantora@hotmail.com>
2019-05-28 11:10:53 -0300

    adiciona exercicios javascript 3

--- exerciciodom/script.js
+++ exerciciodom/script.js
@@ -1,10 +1,22 @@
-let listas = ['pera','uva','tomate','caqui']
+let listas = ['pera','uva','tomate','caqui','morango','banana'];
 let listUl = document.querySelector('ul');
+//let texto = document.querySelector('#text').value;
+let botao = document.querySelector('button');
 
 
+//element serve para printar os itens do array por conta do for of.
 for (element of listas){
     let listLi = document.createElement('li');
     let textElement = document.createTextNode(element);
-    listUl.appendChild(listLi)
-    listLi.appendChild(textElement);
-}
+    listUl.appendChild(listLi);
+    listLi.appendChild(textElement); 
+};
+
+botao.addEventListener("click", function(){
+    let texto = document.querySelector('#text').value;
+    let listaLi = document.createElement('li');
+    let textoInput = document.createTextNode(texto);
+    //primeiro a mãe, depois o filho.
+    listUl.appendChild(listaLi);
+    listaLi.appendChild(textoInput);
+});
